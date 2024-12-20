eaa10e2bc4a8276bbd35bc5c35ae7929173f8966
nengkya <nengkya@yahoo.com>
2024-12-21 04:16:52 +0700

    gcc raylib.c -lraylib

---
+++ raylib.c
@@ -0,0 +1,22 @@
+#include "raylib.h"
+
+int main() {
+
+    InitWindow(800, 450, "HaGa");
+
+    while (!WindowShouldClose()) {
+
+	/*void BeginDrawing(void); setup canvas (framebuffer) to start drawing
+
+	  a framebuffer is a portion of a computer's random-access memory (RAM)
+	  that stores the data for a video frame, or image, to be displayed on the screen
+	
+	*/
+
+
+    }
+
+
+
+
+}
