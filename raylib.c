6a2ab0c17f700ce65f2c8fded2a4b35a356ff151
nengkya <nengkya@yahoo.com>
2024-12-21 17:30:10 +0700

    DrawText(HaGa, 190, 200, 20, LIGHTGRAY);

--- raylib.c
+++ raylib.c
@@ -16,11 +16,15 @@ int main() {
 	/*void ClearBackground(Color color); set background color (framebuffer clear color)
 	  #define RAYWHITE (Color){ 245, 245, 245, 255 }
 	*/
+	ClearBackground(RAYWHITE);
 
+	/*void ImageDrawText(Image * dst, const char * text, int posX, int posY, int fontSize, Color color);
+		    Draw text (using default font) within an image (destination)
+	*/
+	DrawText("DrawText(190, 200, 20, LIGHTGRAY);", 190, 200, 20, LIGHTGRAY);
 
-
-
-	
+	/*void EndDrawing(void); end canvas drawing and swap buffers (double buffering)*/
+	EndDrawing();	
 
     }
 
