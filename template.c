4c62e66e9a67c2fdbf7f61772a36aa2aef97b72e
nengkya <nengkya@yahoo.com>
2025-02-16 17:16:40 +0700

    cursor wont hide, fuck !!!

---
+++ template.c
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+
+int main() {
+
+    const int screenWidth = 1600, screenHeight = 900;
+
+    InitWindow(screenWidth, screenHeight, "Wildan Rhomad Wijanarko");
+
+    while(!WindowShouldClose()) {
+
+
+
+        EndDrawing();
+
+    }
+
+}
