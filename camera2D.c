d1dae1f25beb85f9c8e46480fb69a695937b0e69
hasnagagah <nengkya2@gmail.com>
2025-02-17 16:57:52 +0700

    error: Your local changes to the following files would be overwritten by merge: camera2D.c

--- camera2D.c
+++ camera2D.c
@@ -5,12 +5,12 @@ int main() {
 
     const int screenWidth = 1000, screenHeight = 500;
 
-	typedef struct Camera2D {
-		Vector2 offset;	/*camera offset (displacement from target in screen-space)*/
-		Vector2 target;	/*camera target (focus point in world-space)*/
-		float rotation;	/*camera rotation in degrees*/
-		float zoom;		/*camera zoom (scaling), should not be 0*/
-	} Camera2D;
+    typedef struct Camera2D {
+	Vector2 offset;	/*camera offset (displacement from target in screen-space)*/
+	Vector2 target;	/*camera target (focus point in world-space)*/
+	float rotation;	/*camera rotation in degrees*/
+	float zoom;	/*camera zoom (scaling), should not be 0*/
+    } Camera2D;
 
 
 
