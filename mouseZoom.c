38aaca75b45784c9d93196e0d35ae6032dd2846c
HaGa IT Solution <nengkya@yahoo.com>
2025-02-19 10:43:09 +0700

    set tab to 4

--- mouseZoom.c
+++ mouseZoom.c
@@ -11,43 +11,43 @@ int main() {
 
     while(!WindowShouldClose()) {
 
-	/*typedef enum {KEY_ONE = 49; key: 1} KeyboardKey*/
-
-	/*Vector2 GetMouseDelta(void); get mouse delta between frames*/
-	Vector2 delta = GetMouseDelta();
-	/*
-	scale vector (multiply by value)
-	RMAPI Vector2 Vector2Scale(Vector2 v, float scale) {
-	    Vector2 result = {v.x*scale, v.y*scale}; return result;
-	}
-	*/
-	delta = Vector2Scale(delta, -1 / camera.zoom);
-
-	/*
-	RMAPI Vector2 Vector2Add(Vector2 v1, Vector2 v2) {
-	    Vector2 result = { v1.x + v2.x, v1.y + v2.y };
-	    return result;
-	}
-	*/
-	camera.target = Vector2Add(camera.target, delta);
-	GetMouseWheelMove();
-	/*
-	Vector2 GetScreenToWorld2D(Vector2 position, Camera2D camera);
-	get the world space position for a 2d camera screen space position
-	*/
-	Vector2 GetScreenToWorld2D(GetMousePosition(), camera);
-	camera.offset = GetMousePosition();
-
-	/*start drawing*/
-	BeginDrawing();
-	BeginMode2D(camera);
-
-	/*draw the 3D grid, rotated 90 degrees and centered around 0,0*/
-
-	DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 50, MAROON);
-	EndMode2D();
-
-        EndDrawing();
+		/*typedef enum {KEY_ONE = 49; key: 1} KeyboardKey*/
+	
+		/*Vector2 GetMouseDelta(void); get mouse delta between frames*/
+		Vector2 delta = GetMouseDelta();
+		/*
+		scale vector (multiply by value)
+		RMAPI Vector2 Vector2Scale(Vector2 v, float scale) {
+		    Vector2 result = {v.x*scale, v.y*scale}; return result;
+		}
+		*/
+		delta = Vector2Scale(delta, -1 / camera.zoom);
+	
+		/*
+		RMAPI Vector2 Vector2Add(Vector2 v1, Vector2 v2) {
+		    Vector2 result = { v1.x + v2.x, v1.y + v2.y };
+		    return result;
+		}
+		*/
+		camera.target = Vector2Add(camera.target, delta);
+		GetMouseWheelMove();
+		/*
+		Vector2 GetScreenToWorld2D(Vector2 position, Camera2D camera);
+		get the world space position for a 2d camera screen space position
+		*/
+		Vector2 GetScreenToWorld2D(GetMousePosition(), camera);
+		camera.offset = GetMousePosition();
+	
+		/*start drawing*/
+		BeginDrawing();
+		BeginMode2D(camera);
+	
+		/*draw the 3D grid, rotated 90 degrees and centered around 0,0*/
+	
+		DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 50, MAROON);
+		EndMode2D();
+	
+	    EndDrawing();
 
     }
 
