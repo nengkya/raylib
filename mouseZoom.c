d5e4b7e78f433655023c5b6b8f3d9c2fe0686b1c
nengkya <nengkya@yahoo.com>
2025-02-19 08:50:44 +0700

    mouseZoom.c not drawing

--- mouseZoom.c
+++ mouseZoom.c
@@ -1,22 +1,39 @@
-#include "raylib.h"
-
+#include "raylib.h"	 /*InitWindow();*/
+#include "raymath.h" /*Vector2Add();*/
 
 int main() {
 
     const int screenWidth = 1000, screenHeight = 500;
     InitWindow(screenWidth, screenHeight, "mouse zoom - Wildan Rhomad Wijanarko");
+	Camera2D camera;
 	SetTargetFPS(60);
 
     while(!WindowShouldClose()) {
 
-		/* typedef enum {KEY_ONE = 49; key: 1} KeyboardKey*/
+		/*typedef enum {KEY_ONE = 49; key: 1} KeyboardKey*/
 
-		/*Vector2 GetScreenToWorld2D(Vector2 position, Camera2D camera); get the world space position for a 2d camera screen space position*/
+		/*Vector2 GetMouseDelta(void); get mouse delta between frames*/
+		Vector2 delta = GetMouseDelta();
+		/*
+		RMAPI Vector2 Vector2Add(Vector2 v1, Vector2 v2) {
+			Vector2 result = { v1.x + v2.x, v1.y + v2.y };
+			return result;
+		}
+		*/
+		camera.target = Vector2Add(camera.target, delta);
+
+		camera.offset = GetMousePosition();
+
+		/*Vector2 GetScreenToWorld2D(Vector2 position, Camera2D camera); get the world space position for a 2D camera screen space position*/
 
 
+		/*start drawing*/
+		BeginDrawing();
+		BeginMode2D(camera);
 
+		/*draw the 3D grid, rotated 90 degrees and centered around 0,0*/
 
-
+		DrawCircle(GetScreenWidth() / 2, GetScreenHeight() / 2, 50, MAROON);
 
         EndDrawing();
 
