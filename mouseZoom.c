0dcf656a12850c9db058b7bc38ca333430c402ee
hasnagagah <nengkya2@gmail.com>
2025-02-19 08:53:23 +0700

    mouseZoom.c not drawing from acer.

--- mouseZoom.c
+++ mouseZoom.c
@@ -5,13 +5,16 @@ int main() {
 
     const int screenWidth = 1000, screenHeight = 500;
     InitWindow(screenWidth, screenHeight, "mouse zoom - Wildan Rhomad Wijanarko");
-	SetTargetFPS(60);
+    SetTargetFPS(60);
 
     while(!WindowShouldClose()) {
 
-		/* typedef enum {KEY_ONE = 49; key: 1} KeyboardKey*/
+	/* typedef enum {KEY_ONE = 49; key: 1} KeyboardKey*/
 
-		/*Vector2 GetScreenToWorld2D(Vector2 position, Camera2D camera); get the world space position for a 2d camera screen space position*/
+	/*
+	Vector2 GetScreenToWorld2D(Vector2 position, Camera2D camera);
+	get the world space position for a 2d camera screen space position
+	*/
 
 
 
