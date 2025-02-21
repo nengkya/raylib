182919b021b2c54eec7ab33f15190276fbcf40c1
nengkya <nengkya@yahoo.com>
2025-02-21 11:47:59 +0700

    UpdateCameraPro error. Use update UpdateCamera instead.

--- Camera3DchatGPT.c
+++ Camera3DchatGPT.c
@@ -1,6 +1,9 @@
+#include "stdio.h"
 #include "raylib.h"
 
+
 int main(void) {
+	printf("HaGa raylib, %s\n", RAYLIB_VERSION); /*5.6-dev*/
     InitWindow(800, 600, "raylib Camera3D Example");
     Camera3D camera = { 0 };
     camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
@@ -10,11 +13,23 @@ int main(void) {
     camera.projection = CAMERA_PERSPECTIVE;
     /*SetCameraMode(camera, CAMERA_FREE); free camera mode*/
 
-    while (!WindowShouldClose()) {
-        UpdateCamera(&camera, 1); // Update camera movement
+	Vector3 movement = {0.1f, 0.0f, 0.0f}; /*move right*/
+	Vector3 rotation = {0.0f, 1.0f, 0.0f}; /*rotate horizontally*/
 
-        BeginDrawing();
+
+    while (!WindowShouldClose()) {
+
+		/*
+		UpdateCamera is obsolote
+		void UpdateCameraPro(Camera3D * camera, Vector3 movement, Vector3 rotation, float zoom);
+		*/
+		UpdateCameraPro(&camera, movement, rotation, 0.0f);
+        
+		BeginDrawing();
             ClearBackground(RAYWHITE);
+
+			DrawText(RAYLIB_VERSION, 200, 80, 20, RED);
+
             BeginMode3D(camera);
                 DrawGrid(10, 1.0f);
             EndMode3D();
