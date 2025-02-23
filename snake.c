ecd2d917e96339ba7046a5f74119ab77ed134552
hasnagagah <nengkya2@gmail.com>
2025-02-23 19:11:04 +0700

    DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);

--- snake.c
+++ snake.c
@@ -2,11 +2,8 @@
 #define SQUARE_SIZE 31
 #define SNAKE_LENGTH 256
 
-<<<<<<< HEAD
-=======
 typedef struct Snake {Vector2 position; Vector2 size; Vector2 speed; Color color;} Snake;
 
->>>>>>> 45e308c5ca6824e768eb94ad1e078c1954d19987
 static const int screenWidth = 1300, screenHeight = 650;
 static Vector2 offset;
 static Snake snake[SNAKE_LENGTH];
@@ -30,10 +27,6 @@ void InitGame() {
 
 void UpdateGame() {
 
-/*
-bool CheckCollisionCircles(Vector2 center1, float radius1,
-	Vector2 center2, float radius2); check collision between two circles
-*/
 
 
 }
@@ -41,30 +34,22 @@ bool CheckCollisionCircles(Vector2 center1, float radius1,
 
 void DrawGame() {
 
-<<<<<<< HEAD
 	/*
 	draw (using gl lines
 	void DrawLineV(Vector2 startPosition, Vector2 endPosition, Color color);
 	*/
-	for (int i = 0; i < screenWidth / SQUARE_SIZE + 1; i++) {
-
-		DrawLineV((Vector2){SQUARE_SIZE * i + offset.x/2, offset.y/2},
-				  (Vector2){SQUARE_SIZE * i + offset.x/2, screenHeight - offset.y / 2}, LIGHTGRAY);
-	
-	}
-=======
-	/*void DrawLineV(Vector2 startPosition, Vector2 endPosition, Color color); draw a line (using gl lines)*/
 	for (int i = 0; i < screenWidth / SQUARE_SIZE + 1; i++)
 
-		DrawLineV((Vector2){SQUARE_SIZE * i + offset.x/2, offset.y/2}, (Vector2){SQUARE_SIZE * i + offset.x/2, screenHeight - offset.y / 2}, LIGHTGRAY);
->>>>>>> 45e308c5ca6824e768eb94ad1e078c1954d19987
+		DrawLineV((Vector2){SQUARE_SIZE * i + offset.x / 2, offset.y / 2},
+				  (Vector2){SQUARE_SIZE * i + offset.x / 2, screenHeight - offset.y / 2}, LIGHTGRAY);
+	
+	for (int i = 0; i < screenHeight/SQUARE_SIZE + 1; i++)
+		DrawLineV((Vector2){offset.x / 2, SQUARE_SIZE * i + offset.y / 2},
+				  (Vector2){screenWidth - offset.x / 2, SQUARE_SIZE * i + offset.y / 2}, LIGHTGRAY);
 
-	for (int i = 0; i < screenHeight / SQUARE_SIZE + 1; i++)
 
-		DrawLineV((Vector2){offset.x / 2, SQUARE_SIZE*i + offset.y / 2},
-				  (Vector2){screenWidth - offset.x / 2, SQUARE_SIZE*i + offset.y / 2}, LIGHTGRAY);
-
-	for (int i = 0; i < tailCounter; i++) DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);
+	for (int i = 0; i < tailCounter; i++)
+		DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);
 
 
 
