45e308c5ca6824e768eb94ad1e078c1954d19987
nengkya <nengkya@yahoo.com>
2025-02-23 16:34:36 +0700

    for (int i = 0; i < counterTail; i++) DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);

--- snake.c
+++ snake.c
@@ -1,21 +1,26 @@
 #include "raylib.h"
 #define SQUARE_SIZE 31
+#define SNAKE_LENGTH 256
 
-/*typedef struct Rectangle {
-    float x;      X position of the top-left corner
-    float y;      Y position of the top-left corner
-    float width;
-    float height;
-} Rectangle;*/
+typedef struct Snake {Vector2 position; Vector2 size; Vector2 speed; Color color;} Snake;
 
 static const int screenWidth = 1300, screenHeight = 650;
 static Vector2 offset;
+static Snake snake[SNAKE_LENGTH];
+static int tailCounter = 1;
 
 void InitGame() {
 
 	offset.x = screenWidth  % SQUARE_SIZE;
     offset.y = screenHeight % SQUARE_SIZE;
 
+	for (int i = 0; i < SNAKE_LENGTH; i++) {
+
+        snake[i].position = (Vector2){offset.x /2, offset.y / 2};
+        snake[i].size	  = (Vector2){SQUARE_SIZE, SQUARE_SIZE};
+        snake[i].color	  = DARKBLUE;
+    
+	}
 
 }
 
@@ -31,17 +36,17 @@ void UpdateGame() {
 void DrawGame() {
 
 	/*void DrawLineV(Vector2 startPosition, Vector2 endPosition, Color color); draw a line (using gl lines)*/
-	for (int i = 0; i < screenWidth / SQUARE_SIZE + 1; i++) {
+	for (int i = 0; i < screenWidth / SQUARE_SIZE + 1; i++)
 
 		DrawLineV((Vector2){SQUARE_SIZE * i + offset.x/2, offset.y/2}, (Vector2){SQUARE_SIZE * i + offset.x/2, screenHeight - offset.y / 2}, LIGHTGRAY);
-	
-	}
 
-	for (int i = 0; i < screenHeight / SQUARE_SIZE + 1; i++) {
+	for (int i = 0; i < screenHeight / SQUARE_SIZE + 1; i++)
 
 		DrawLineV((Vector2){offset.x / 2, SQUARE_SIZE*i + offset.y / 2}, (Vector2){screenWidth - offset.x / 2, SQUARE_SIZE*i + offset.y / 2}, LIGHTGRAY);
 
-	}
+	for (int i = 0; i < tailCounter; i++) DrawRectangleV(snake[i].position, snake[i].size, snake[i].color);
+
+
 
 }
 
