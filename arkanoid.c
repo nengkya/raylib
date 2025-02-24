867412e700c5ac7f34015f66368f7ffefc1bfe60
hasnagagah <nengkya2@gmail.com>
2025-02-24 18:18:53 +0700

    (ball.position.x + ball.radius) < (brick[i][j].position.x - brickSize.x/2 + ball.speed.x)

--- arkanoid.c
+++ arkanoid.c
@@ -21,6 +21,7 @@ void InitGame() {
 
 	ball.position = (Vector2){player.position.x, player.position.y - player.size.y / 2 - ball.radius };
     ball.radius   = 7;
+	ball.active	  = true;
 
 	brickSize = (Vector2){GetScreenWidth() / BRICKS_PER_LINE, 40 };
 	int initialDownPosition = 50;
@@ -41,14 +42,64 @@ void InitGame() {
 
 void UpdateGame() {
 
+	if (IsKeyDown(KEY_LEFT)) player.position.x -= 5;
+	if ((player.position.x - player.size.x/2) <= 0) player.position.x = player.size.x/2;
+	if (IsKeyDown(KEY_RIGHT)) player.position.x += 5;
+	if ((player.position.x + player.size.x/2) >= screenWidth)
+		player.position.x = screenWidth - player.size.x/2;
+ 
+	ball.speed = (Vector2){0, -5};
+	ball.position.x += ball.speed.x;
+	ball.position.y += ball.speed.y;
 
-
+	for (int i = 0; i < LINES_OF_BRICKS; i++) {
+		for (int j = 0; j < BRICKS_PER_LINE; j++)
+		{
+			if (brick[i][j].active)
+			{
+				// Hit below
+				if (((ball.position.y - ball.radius) <= (brick[i][j].position.y + brickSize.y/2)) &&
+					((ball.position.y - ball.radius) > (brick[i][j].position.y + brickSize.y/2 + ball.speed.y)) &&
+					((fabs(ball.position.x - brick[i][j].position.x)) < (brickSize.x/2 + ball.radius*2/3)) && (ball.speed.y < 0))
+				{
+					brick[i][j].active = false;
+					ball.speed.y *= -1;
+				}
+				// Hit above
+				else if (((ball.position.y + ball.radius) >= (brick[i][j].position.y - brickSize.y/2)) &&
+						((ball.position.y + ball.radius) < (brick[i][j].position.y - brickSize.y/2 + ball.speed.y)) &&
+						((fabs(ball.position.x - brick[i][j].position.x)) < (brickSize.x/2 + ball.radius*2/3)) && (ball.speed.y > 0))
+				{
+					brick[i][j].active = false;
+					ball.speed.y *= -1;
+				}
+				// Hit left
+				else if (((ball.position.x + ball.radius) >= (brick[i][j].position.x - brickSize.x/2)) &&
+						((ball.position.x + ball.radius) < (brick[i][j].position.x - brickSize.x/2 + ball.speed.x)) &&
+						((fabs(ball.position.y - brick[i][j].position.y)) < (brickSize.y/2 + ball.radius*2/3)) && (ball.speed.x > 0))
+				{
+					brick[i][j].active = false;
+					ball.speed.x *= -1;
+				}
+				// Hit right
+				else if (((ball.position.x - ball.radius) <= (brick[i][j].position.x + brickSize.x/2)) &&
+						((ball.position.x - ball.radius) > (brick[i][j].position.x + brickSize.x/2 + ball.speed.x)) &&
+						((fabs(ball.position.y - brick[i][j].position.y)) < (brickSize.y/2 + ball.radius*2/3)) && (ball.speed.x < 0))
+				{
+					brick[i][j].active = false;
+					ball.speed.x *= -1;
+				}
+			}
+		}
+	}
 
 
 }
 
 
 void DrawGame() {
+
+	ClearBackground(BLACK);
 
 	DrawRectangle(player.position.x - player.size.x / 2, player.position.y - player.size.y / 2,
 		player.size.x, player.size.y, YELLOW);
@@ -61,14 +112,13 @@ void DrawGame() {
 			if (brick[i][j].active) {
 				
 				if ((i + j) % 2 == 0)
-					 DrawRectangle(brick[i][j].position.x - brickSize.x/2, brick[i][j].position.y - brickSize.y/2, brickSize.x, brickSize.y, GRAY);
-				else DrawRectangle(brick[i][j].position.x - brickSize.x/2, brick[i][j].position.y - brickSize.y/2, brickSize.x, brickSize.y, DARKGRAY);
+					 DrawRectangle(brick[i][j].position.x - brickSize.x/2,
+						brick[i][j].position.y - brickSize.y/2, brickSize.x, brickSize.y, GRAY);
+				else DrawRectangle(brick[i][j].position.x - brickSize.x/2,
+						brick[i][j].position.y - brickSize.y/2, brickSize.x, brickSize.y, DARKGRAY);
 			}
 		}
 	}
-
-
-
 
 	EndDrawing();
 
